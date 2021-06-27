e6b31fcd2b424f66568bedb2f9a50770bbc888dd
Samuel Adebayo <kurdzsamuel@gmail.com>
2021-06-27 18:21:02 +0100

    colors and shapes

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -1,11 +1,30 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react';
+import { shapes, colors } from '../../mock-data';
 import './filters.scss';
 
 const Filters = () => {
   return (
     <div>
-      <p>Filters</p>
+      <h2>Filters</h2>
+
+      <section className="shapes-container">
+        <h4>Shapes</h4>
+        <div>
+          {shapes.map((shape) => (
+            <p>{shape}</p>
+          ))}
+        </div>
+      </section>
+
+      <section className="colors-container">
+        <h4>Colors</h4>
+        <div>
+          {colors.map((color) => (
+            <p className={color} />
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
