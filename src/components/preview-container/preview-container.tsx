6b41109995808d69d3911522d10172e17b905290
Samuel Adebayo <kurdzsamuel@gmail.com>
2021-06-29 03:52:37 +0100

    action creator effect works on all objects of shapes and colors

--- src/components/preview-container/preview-container.tsx
+++ src/components/preview-container/preview-container.tsx
@@ -1,25 +1,40 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react';
 import './preview-container.scss';
+import { connect } from 'react-redux';
+import { ColorInterface, ShapeInterface } from '../../mock-data';
 
-import { newColorData, newShapeData } from '../../mock-data';
+interface FiltersProps {
+  colors: ColorInterface[];
+  shapes: ShapeInterface[];
+}
 
-const PreviewContainer = () => {
+const PreviewContainer = (props: FiltersProps) => {
+  const { colors, shapes } = props;
   return (
     <div className="preview-container">
       <h2>All Items</h2>
 
       <section className="items">
-        {newShapeData.map((shapeObject) =>
-          newColorData.map((colorObject) => (
-            <div key={`${shapeObject.shape} ${colorObject.color}`}>
-              <div className={`${shapeObject.shape} ${colorObject.color} item`} />
-            </div>
-          )),
+        {shapes.map((shapeObject) =>
+          shapeObject.selected
+            ? colors.map((colorObject) =>
+                colorObject.selected ? (
+                  <div key={`${shapeObject.shape} ${colorObject.color}`}>
+                    <div className={`${shapeObject.shape} ${colorObject.color} item`} />
+                  </div>
+                ) : null,
+              )
+            : null,
         )}
       </section>
     </div>
   );
 };
 
-export default PreviewContainer;
+const mapStateToProps = (state: any) => ({
+  colors: state.filter.colors,
+  shapes: state.filter.shapes,
+});
+
+export default connect(mapStateToProps)(PreviewContainer);
