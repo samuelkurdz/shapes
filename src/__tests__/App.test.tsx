427b18d97f89fa80e24e67043304f469604f9a86
Samuel Adebayo <kurdzsamuel@gmail.com>
2021-06-30 09:01:35 +0100

    Landing page test suite completed

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,17 +1,36 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 import LoginPage from '../pages/login/login';
 import App from '../components/App';
 import Root from '../root';
+import HomePage from '../pages/home/home';
+import Header from '../components/header/header';
 
-it('should render', () => {
-  const wrapped = mount(
-    <Root>
-      <App />
-    </Root>,
-  );
+describe('Before User logs in', () => {
+  let wrapped: ReactWrapper<React.Component<{}>>;
 
-  expect(wrapped.find(LoginPage).length).toEqual(1);
-  wrapped.unmount();
+  beforeEach(() => {
+    wrapped = mount(
+      <Root>
+        <App />
+      </Root>,
+    );
+  });
+
+  afterEach(() => {
+    wrapped.unmount();
+  });
+
+  it('Login page is visible', () => {
+    expect(wrapped.find(LoginPage).length).toEqual(1);
+  });
+
+  it('Header is not displayed', () => {
+    expect(wrapped.find(Header).length).toEqual(0);
+  });
+
+  it('Homepage is not displayed', () => {
+    expect(wrapped.find(HomePage).length).toEqual(0);
+  });
 });
